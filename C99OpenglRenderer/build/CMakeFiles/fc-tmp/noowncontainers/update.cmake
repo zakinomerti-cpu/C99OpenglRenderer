4a8cc4d140e99e7abda59a194c6e40ffcd3f0250
Zakinomerti-cpu <zakinomerti@gmail.com>
2026-09-25 03:31:27 +0700

    restructure

--- C99OpenglRenderer/build/CMakeFiles/fc-tmp/noowncontainers/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for noowncontainers")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/xinitrix/projects/C99OpenglRenderer/C99OpenglRenderer/build/CMakeFiles/fc-tmp/noowncontainers/noowncontainers-gitupdate.cmake")
-
-endblock()
